d4f0d818a804a4d40f29bb101c8be3f416b7e7ed
cornelsiclovan <cornel.siclovan@gmail.com>
2021-07-08 15:45:48 +0300

    product detail page

--- src/products/components/ProductMain.js
+++ src/products/components/ProductMain.js
@@ -30,6 +30,8 @@ const ProductMain = (props) => {
         console.log(event.target.dataset.doc_name);
     }
 
+    const firstImage = props.product.image.shift();
+
     return (
         <React.Fragment>
             <div className="continue-shopping"> 
@@ -46,10 +48,22 @@ const ProductMain = (props) => {
                 <h2> {props.product.description} </h2>
             </div>
 
-            {props.product.image.map(image => {
-                 return <img style={{maxWidth: '300px'}}  className="card-item__picture" src={"http:\\\\localhost:3001\\"+image} />
-            })}
+            <div style={{float: 'left', width: '100%'}}> 
+                
+                <img style={{width: '100%', border: '1px solid lightgrey', padding: '5px'}}  className="card-item__picture" src={"http:\\\\localhost:3001\\"+firstImage} />
 
+                <br/>
+
+                {props.product.image.map(image => {
+                    
+                    return <img style={{maxWidth: '13%', border: '1px solid lightgrey'}}  className="card-item__picture" src={"http:\\\\localhost:3001\\"+image} />
+                })}
+
+            </div> 
+
+           
+                <h1>Related documents</h1>
+        
             <br />
 
 
@@ -84,8 +98,24 @@ const ProductMain = (props) => {
                    
                 })
             }
-        
+
             <br />
+         
+            
+            <div style={{clear: 'both', width: '50%'}}>
+                <br />
+                <h1>Parameters</h1>
+                <h2>Model <span style={{float: 'right'}}>{props.product.name}</span></h2>
+                <h2>SKU  <span style={{float: 'right'}}>{props.product.sku}</span></h2>
+                <h2>Bus Power  <span style={{float: 'right'}}>{props.product.bus_power}</span></h2>
+                <h2>Width  <span style={{float: 'right'}}>{props.product.width}</span></h2>
+                <h2>Height  <span style={{float: 'right'}}>{props.product.height}</span></h2>
+                <h2>Depth  <span style={{float: 'right'}}>{props.product.depth}</span></h2>
+                <h2>Weight  <span style={{float: 'right'}}>{props.product.weight}</span></h2>
+                <h2>Discount categort  <span style={{float: 'right'}}>{props.product.discountCategory}</span></h2>
+            < br/> 
+            </div>
+            
         </React.Fragment>
     )
 }
